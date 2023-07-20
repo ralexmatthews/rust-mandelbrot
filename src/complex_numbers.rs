34bf510490c57a485a629e72c017c02b0d58d344
Alex Matthews <amatthews4851@gmail.com>
2023-07-20 02:03:01 -0500

    Implements Mandelbrot

---
+++ src/complex_numbers.rs
@@ -0,0 +1,35 @@
+pub struct ComplexNumber {
+    pub real: f32,
+    pub imaginary: f32,
+}
+
+// fn numbers_are_equal(number1: &ComplexNumber, number2: &ComplexNumber) -> bool {
+//     number1.real == number2.real && number1.imaginary == number2.imaginary
+// }
+
+pub fn get_magnitude(complex_number: &ComplexNumber) -> f32 {
+    (complex_number.real * complex_number.real)
+        + (complex_number.imaginary * complex_number.imaginary)
+}
+
+pub fn square_complex_number(number: &ComplexNumber) -> ComplexNumber {
+    // (r1 * r1) + (r1 * i2) + (i1 + r2) + (i1 * i2)
+    ComplexNumber {
+        real: (number.real * number.real) - (number.imaginary * number.imaginary),
+        imaginary: (number.real * number.imaginary) * 2.0,
+    }
+}
+
+pub fn add_complex_numbers(number1: &ComplexNumber, number2: &ComplexNumber) -> ComplexNumber {
+    ComplexNumber {
+        real: number1.real + number2.real,
+        imaginary: number1.imaginary + number2.imaginary,
+    }
+}
+
+pub fn zero() -> ComplexNumber {
+    ComplexNumber {
+        real: 0.0,
+        imaginary: 0.0,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,81 @@
+#[macro_use]
+extern crate bmp;
+use bmp::{Image, Pixel};
+
+mod complex_numbers;
+
+fn iterate(
+    base: &complex_numbers::ComplexNumber,
+    operand: &complex_numbers::ComplexNumber,
+) -> complex_numbers::ComplexNumber {
+    complex_numbers::add_complex_numbers(&complex_numbers::square_complex_number(operand), base)
+}
+
+fn find_converges(
+    base: complex_numbers::ComplexNumber,
+    operand: complex_numbers::ComplexNumber,
+    iteration: u8,
+) -> Option<u8> {
+    if iteration >= 255 {
+        return None;
+    }
+    let result = iterate(&base, &operand);
+
+    let magnitude = complex_numbers::get_magnitude(&result);
+    if magnitude >= 2.0 {
+        return Some(iteration);
+    }
+
+    find_converges(base, result, iteration + 1)
+}
+
+fn map_i32_to_pixel(value: u8) -> Pixel {
+    px!(255, 255 - value, 255 - value)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let out_of_bounds: f32 = 2.0;
+    let resolution: u16 = 24000;
+
+    let mut image = Image::new(u32::from(resolution), u32::from(resolution));
+
+    let half_of_resolution = i16::try_from(resolution).unwrap() / 2;
+
+    let float_half_of_resolution = f32::from(half_of_resolution);
+
+    for y in -half_of_resolution..half_of_resolution {
+        for x in -half_of_resolution..half_of_resolution {
+            let complex_number = complex_numbers::ComplexNumber {
+                real: (f32::from(x) / float_half_of_resolution) * out_of_bounds,
+                imaginary: (f32::from(y) / float_half_of_resolution) * out_of_bounds,
+            };
+
+            let result = find_converges(complex_number, complex_numbers::zero(), 0);
+
+            let ux = u32::try_from(x + half_of_resolution).unwrap();
+            let uy = u32::try_from(y + half_of_resolution).unwrap();
+
+            match result {
+                Some(x) => image.set_pixel(ux, uy, map_i32_to_pixel(x)),
+                None => image.set_pixel(ux, uy, px!(0, 0, 0)),
+            }
+        }
+    }
+
+    let u_half = u32::from(resolution / 2);
+    for x in 0..u32::from(resolution) {
+        image.set_pixel(u_half, x, px!(0, 0, 0));
+        image.set_pixel(u_half + 1, x, px!(0, 0, 0));
+        image.set_pixel(u_half - 1, x, px!(0, 0, 0));
+        image.set_pixel(u_half + 2, x, px!(0, 0, 0));
+        image.set_pixel(u_half - 2, x, px!(0, 0, 0));
+
+        image.set_pixel(x, u_half, px!(0, 0, 0));
+        image.set_pixel(x, u_half + 1, px!(0, 0, 0));
+        image.set_pixel(x, u_half - 1, px!(0, 0, 0));
+        image.set_pixel(x, u_half + 2, px!(0, 0, 0));
+        image.set_pixel(x, u_half - 2, px!(0, 0, 0));
+    }
+
+    let _ = image.save("fractal.bmp");
 }
