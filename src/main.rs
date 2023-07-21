a84fcd23c86c0672a7b3a10ea6e552ffe9b917f8
Alex Matthews <amatthews4851@gmail.com>
2023-07-21 12:00:42 -0500

    Gets multithreading working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate bmp;
 use bmp::{Image, Pixel};
+use std::thread;
 
 mod complex_numbers;
 
@@ -11,7 +12,8 @@ fn iterate(
     base: complex_numbers::ComplexNumber,
     operand: complex_numbers::ComplexNumber,
 ) -> complex_numbers::ComplexNumber {
-    complex_numbers::add_complex_numbers(complex_numbers::square_complex_number(operand), base)
+    let operand_squared = complex_numbers::square_complex_number(operand);
+    complex_numbers::add_complex_numbers(operand_squared, base)
 }
 
 // Returns the number of iterations it took to either exceed two or repeat a number
@@ -44,12 +46,13 @@ fn find_converges(
 
 // maps a number between 0 and 1024 to a pixel color
 fn map_iterations_to_pixel(value: u16) -> Pixel {
-    px!(255, 255 - (value / 4), 255 - (value / 4))
+    let mapped_value = 255 - (value / 4);
+    px!(mapped_value, 255, mapped_value)
 }
 
 fn main() {
     let out_of_bounds: f32 = 2.0;
-    let resolution: u16 = 16000;
+    let resolution: u16 = 4000;
 
     let mut image = Image::new(u32::from(resolution), u32::from(resolution));
 
@@ -57,22 +60,65 @@ fn main() {
 
     let float_half_of_resolution = f32::from(half_of_resolution);
 
-    for y in -half_of_resolution..half_of_resolution {
-        for x in -half_of_resolution..half_of_resolution {
-            let complex_number = complex_numbers::ComplexNumber {
-                real: (f32::from(x) / float_half_of_resolution) * out_of_bounds,
-                imaginary: (f32::from(y) / float_half_of_resolution) * out_of_bounds,
-            };
+    let thread1 = thread::spawn(move || {
+        let mut pixels = vec![];
+        for y in -half_of_resolution..-1 {
+            let mut row = vec![];
+            for x in -half_of_resolution..half_of_resolution {
+                let complex_number = complex_numbers::ComplexNumber {
+                    real: (f32::from(x) / float_half_of_resolution) * out_of_bounds,
+                    imaginary: (f32::from(y) / float_half_of_resolution) * out_of_bounds,
+                };
 
-            let result = find_converges(complex_number, complex_numbers::zero(), 0);
+                let result = find_converges(complex_number, complex_numbers::zero(), 0);
 
-            let ux = u32::try_from(x + half_of_resolution).unwrap();
-            let uy = u32::try_from(y + half_of_resolution).unwrap();
+                let pixel = match result {
+                    Some(x) => map_iterations_to_pixel(x),
+                    None => px!(0, 0, 0),
+                };
 
-            match result {
-                Some(x) => image.set_pixel(ux, uy, map_iterations_to_pixel(x)),
-                None => image.set_pixel(ux, uy, px!(0, 0, 0)),
+                row.push(pixel);
             }
+            pixels.push(row);
+        }
+        pixels
+    });
+    let thread2 = thread::spawn(move || {
+        let mut pixels = vec![];
+        for y in 0..half_of_resolution {
+            let mut row = vec![];
+            for x in -half_of_resolution..half_of_resolution {
+                let complex_number = complex_numbers::ComplexNumber {
+                    real: (f32::from(x) / float_half_of_resolution) * out_of_bounds,
+                    imaginary: (f32::from(y) / float_half_of_resolution) * out_of_bounds,
+                };
+
+                let result = find_converges(complex_number, complex_numbers::zero(), 0);
+
+                let pixel = match result {
+                    Some(x) => map_iterations_to_pixel(x),
+                    None => px!(0, 0, 0),
+                };
+
+                row.push(pixel);
+            }
+            pixels.push(row);
+        }
+        pixels
+    });
+
+    let top_pixels = thread1.join().unwrap();
+    let bottom_pixels = thread2.join().unwrap();
+
+    let all_pixels = top_pixels
+        .iter()
+        .chain(bottom_pixels.iter())
+        .collect::<Vec<_>>();
+
+    for y in 0..(resolution - 1) {
+        for x in 0..(resolution - 1) {
+            let pixel = all_pixels[y as usize][x as usize];
+            image.set_pixel(u32::try_from(x).unwrap(), u32::try_from(y).unwrap(), pixel);
         }
     }
 
